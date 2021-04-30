3eb020bd6e727998eb0c847744f3c688ea7a704a
instinct <78747877+rootinstinct@users.noreply.github.com>
2021-04-30 09:55:43 +0200

    BTT Preview Release
    first proper commit to code in months

--- bettertimetable.py
+++ bettertimetable.py
@@ -1,103 +1,169 @@
-#   BetterTimetable: Version 0.2 Preview.
-#   Configuration added! If your Timetable is not configured you should automatically be asked to go through the setup process.
-#   Made by rootinstinct, https://www.github.com/rootinstinct/ :)
-#   Let me know if anything is broken too. Constantly updating and squashing out bugs.
-print("BetterTimetable -- Alpha 2 / 0.2")
-config = open("config.txt","r") 
-config2 = int(config.readline())
-config.close()
-config = open("config.txt", "w")
-if config2 == 1:
-    print("BetterTimetable setup initialized, answer a few questions and we'll get you up and running.")
-    username = config.writelines(input("What is your name?"))
-    subjectone_dayone = config.writelines((input("What is the first subject on Day One?")))
-    subjecttwo_dayone = config.writelines((input("What is your second subject on Day One?")))
-    subjectthree_dayone = config.writelines((input("What is your third subject on Day One?")))
-    subjectfour_dayone = config.writelines((input("What is your fourth subject on Day One?")))
-    subjectfive_dayone = config.writelines((input("What is your fifth subject on Day One?")))
-            #   Day Two
-    subjectone_daytwo = config.writelines((input("What is the first subject on Day Two?")))
-    subjecttwo_daytwo = config.writelines((input("What is your second subject on Day Two?")))
-    subjectthree_daytwo = config.writelines((input("What is your third subject on Day Two?")))
-    subjectfour_daytwo = config.writelines((input("What is your fourth subject on Day Two?")))
-    subjectfive_daytwo = config.writelines((input("What is your fifth subject on Day Two?")))
-            #   Day Three
-    subjectone_daythree = config.writelines((input("What is the first subject on Day Three?")))
-    subjecttwo_daythree = config.writelines((input("What is your second subject on Day Three?")))
-    subjectthree_daythree = config.writelines((input("What is your third subject on Day Three?")))
-    subjectfour_daythree = config.writelines((input("What is your fourth subject on Day Three?")))
-    subjectfive_daythree = config.writelines((input("What is your fifth subject on Day Three?")))
-            #   Day Four
-    subjectone_dayfour = config.writelines((input("What is the first subject on Day Four?")))
-    subjecttwo_dayfour = config.writelines((input("What is your second subject on Day Four?")))
-    subjectthree_dayfour = config.writelines((input("What is your third subject on Day Four?")))
-    subjectfour_dayfour = config.writelines((input("What is your fourth subject on Day Four?")))
-    subjectfive_dayfour = config.writelines((input("What is your fifth subject on Day Four?")))
-            #   Day Five
-    subjectone_dayfive = config.writelines((input("What is the first subject on Day Five?")))
-    subjecttwo_dayfive = config.writelines((input("What is your second subject on Day Five?")))
-    subjectthree_dayfive = config.writelines((input("What is your third subject on Day Five?")))
-    subjectfour_dayfive = config.writelines((input("What is your fourth subject on Day Five?")))
-    subjectfive_dayfive = config.writelines((input("What is your fifth subject on Day Five?")))
-            #   Day Six
-    subjectone_daysix = config.writelines((input("What is the first subject on Day Six?")))
-    subjecttwo_daysix = config.writelines((input("What is your second subject on Day Six?")))
-    subjectthree_daysix = config.writelines((input("What is your third subject on Day Six?")))
-    subjectfour_daysix = config.writelines((input("What is your fourth subject on Day Six?")))
-    subjectfive_daysix = config.writelines((input("What is your fifth subject on Day Six?")))
-    config.write("0")
-    print("Alright, thats all the setup! This is BetterTimetable.")
-else:
-            print("BetterTimetable configured.")
-day1 = "Day 1: (Unconfigured)"
-day2 = "Day 2: (Unconfigured)"
-day3 = "Day 3: (Unconfigured)"
-day4 = "Day 4: (Unconfigured)"
-day5 = "Day 5: (Unconfigured)"
-day6 = "Day 6: (Unconfigured)"
-day0 = "Day 0: (Unconfigured)"
-day11 = "Day One: [Subjects]\n[Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-day21 = "Day Two: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-day31 = "Day Three: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-day41 = "Day Four: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-day51 = "Day Five: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-day61 = "Day Six: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
-day01 = "Day Zero: [Subject 1]\n[Subject 2]\n[Subject 3]\n[Subject 4]\n[Subject 5]"
+#   BTT: Version 0.2 Revival [Preview Release]
+#   This release is incomplete. Only contains the Setup and could contain bugs.
+#   Build Version: 4
+#   Build Date: April 30, 2021
+#   Made by rootinstinct, https://github.com/rootinstinct/ :) 
+#   Special thanks to https://github.com/lemonheadongit/, they really helped out. (Literally redid the project 1000x better than me.)
+#   Imports
+
+import time
+import os
+import datetime
+import random
+
+#   Config Check and FTS
+
 print()
-config = input("Timetable view:\nWould you like Clear View [press 0] or the default view? [press 1] ")
-open1 = open("config.txt", "r")
-if day1 == "[Day 1] (Unconfigured)":
-    print("Status -- Unconfigured, please change code.")
+print("================================")
+print("|| BTT Revival 0.2 // Alpha 2 ||")
+print("================================")
+print("||    Made by rootinstinct    ||")
+print("================================")
+print("If you're experiencing any problems, be sure to leave an issue in the repository and let us know. \nRepo Link: https://github.com/rootinstinct/bettertimetable\n")
+time.sleep(3)
+
+cfgcheck = open('config.txt', 'r')
+with open('config.txt', 'r') as cfgcheck:
+    cfgstatus = cfgcheck.readlines()
+
+#   First Time Setup [FTS]
+
+if cfgstatus == ['fts0']:
+    print("Starting First Time Setup...")
+    time.sleep(1)
+    input("First Time Setup ready, press any button to get started.\n")
+
+#   Phase One:
+#   Username
+
+usersetup = open('username.txt', 'w')
+with open('username.txt', 'w') as usersetup:
+    username = input("We best get started with your name. Type out your name.\n")
+
+#   Username Confirm
+
+userconfirm = open('username.txt', 'w')
+with open('username.txt', 'w') as userconfirm:
+    print()
+    print("Alright,", username)
+    userconfirmin = input("Is that correct? [y/n]\n")
+
+if userconfirmin == "n":
+    userconfirmin = 1
+
+if userconfirmin == "y":
+    userconfirmin = 0
+
+usertruewrite = open('username.txt', 'w')
+if userconfirmin == 0:
+    with open('username.txt', 'w') as usertruewrite:
+        usertruewrite.write(username)
+
+#   Username Confirm: Loop
+
+while userconfirmin == 1:
+    usersetup = open('username.txt', 'w')
+    with open('username.txt', 'w') as usersetup:
+        username = input("Alright, we go again, your name?\n")
+        usersetup.write(username)
+    with open('username.txt', 'r') as userconfirm:
+        print("Alright,", username)
+        userconfirmin = input("Is that correct? [y/n]\n")
+
+#   Amount Of Days [AOD]
+
+print()
+
+aod = input("Now that we got your name sorted out, how many days of work do you have in a week? (This cannot be more than 7)\n")
+
+aod = int(aod)
+
+#   AOD Confirm
+
+print()
+
+print("Alright, you have", aod, " days in your schedule.")
+aodconfirm = input("Is that correct? [y/n]\n")
+
+if aodconfirm == "n":
+    aodconfirm = 1
+
+#   AOD Confirm: Loop
+
+while aodconfirm == 1:
+    aod = input("Let's try get it right this time (no pressure), how much days do you have per week in your schedule (1-7)\n")
+    print("Alright, you have", aod, ".\n")
+    aodconfirm = input("Is that correct? [y/n]\n")
+    if aodconfirm == "n":
+        aodconfirm = 1
+    else:
+        aodconfirm = 0
+
+#   AOD Check
+
+aod = int(aod)
+
+if aod > 7 or aod < 1:
+    input("That input was incorrect, amount of days cannot be more than 7 or less than 1. Press any button to continue.\n")
+    aoderror = 1
 else:
-    print("Select the day and we'll show you your schedule.")
-day = input()
-day = int(day)
-if day == 1 and config == 1:
-    print(day1)
-else:
-    print(day11)
-if day == 2 and config == 1:
-    print(day2)
-else:
-    print(day21)
-if day == 3 and config == 1:
-    print(day3)
-else:
-    print(day31)
-if day == 4 and config == 1:
-    print(day4)
-else:
-    print(day41)
-if day == 5 and config == 1:
-    print(day5)
-else:
-    print(day51)
-if day == 6 and config == 1:
-    print(day6)
-else:
-    print(day61)
-if day == 0 and config == 1:
-    print(day0)
-else:
-    print(day01)
-#   Hope your timetable is better :D
+    aoderror = 0
+
+#   AOD Check: Loop
+
+while aoderror == 1:
+    aod = input("Let's try get it right this time (no pressure), how much days do you have per week in your schedule (1-7)\n")
+    aod = int(aod)
+    if aod > 7 or aod < 1:
+        input("That input was incorrect, amount of days cannot be more than 7 or less than 1. Press any button to continue.\n")
+    else:
+        aoderror = 0
+
+#   AOD Write
+
+aodwrite = open('aod.txt', 'w')
+
+aod = str(aod)
+
+if aoderror == 0:
+    with open('aod.txt', 'w') as aodwrite:
+        aodwrite.write(aod)
+
+#   Amount of Subjects Per Day [AOS]
+
+aos = input("And before we get into Subject setup, how many subjects do you have per day?\n")
+
+aos = int(aos)
+
+#   AOS Confirm
+
+print("Alright, you have", aos, "subjects per day.")
+aosconfirm = input("Is that correct? [y/n]")
+
+if aosconfirm == "n":
+    aosconfirm = 1
+
+if aosconfirm == "y":
+    aosconfirm = 0
+
+#   AOS Confirm: Loop
+
+while aosconfirm == 1:
+    aos = input("Alright, let's try again. How many subjects do you have per day?\n")
+    aos = int(aos)
+    print("So, you have", aos, "subjects.\n")
+    aosconfirmin = input("Is that correct? [y/n]")
+    if aosconfirmin == "n":
+        aosconfirm = 1
+
+#   AOS Write
+
+aoswrite = open('aos.txt', 'w')
+
+aos = str(aos)
+
+if aosconfirm == 0:
+    with open('aos.txt', 'w') as aoswrite:
+        aoswrite.write(aos)
+
+#   End of Preview
